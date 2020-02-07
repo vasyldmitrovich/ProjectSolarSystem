92df735545764a9b927f47a6bd3e77064c2245ba
VovaProtsiv <vovapro111@gmail.com>
2020-02-07 20:41:34 +0200

    add telegram bot

--- src/main/java/org/solarsystem/telegrambot/TelegramBot.java
+++ src/main/java/org/solarsystem/telegrambot/TelegramBot.java
@@ -1,6 +1,51 @@
 package org.solarsystem.telegrambot;
 
-public class TelegramBot {
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-    // TODO: implement bot here
+public class TelegramBot extends TelegramLongPollingBot{
+
+    public static void main (String args[]) {
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new TelegramBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(update.getMessage().getText());
+
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(stringBuilder.reverse().toString());
+            try {
+
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+
+        return "PlanetaryFactBot";
+    }
+
+
+    public String getBotToken() {
+
+        return "1093856664:AAFcU9c_fp043gXMUsoDrkjsoxLwHp0Gafg";
+    }
 }
