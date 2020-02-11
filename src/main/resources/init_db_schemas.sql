acd6b9fc9d23f53c9013406bd4f76068177c41ef
VasylBazhan <Rapidvasyaalister@gmail.com>
2020-02-11 16:10:41 +0200

    Finished SQL Script... add if not exists date

--- src/main/resources/init_db_schemas.sql
+++ src/main/resources/init_db_schemas.sql
@@ -43,7 +43,28 @@ create table if not exists solar_system.images
             on delete cascade
 );
 
-INSERT INTO solar_system.images (path_to_the_file, id_planet) VALUES ('/resources/pictures/first_image_mercury.jpg', 1);
+INSERT INTO solar_system.images (path_to_the_file, id_planet)
+SELECT * FROM (SELECT '/resources/pictures/first_image_mercury.jpg', 1) AS tmp
+                WHERE NOT EXISTS (
+                    SELECT path_to_the_file FROM solar_system.images WHERE
+                        path_to_the_file='/resources/pictures/first_image_mercury.jpg'
+                    ) LIMIT 1;
 
-INSERT INTO solar_system.planets (name, orbital_period, diameter, gravity, is_satellites, short_description, full_description, language_id) VALUES ('mercury', 88, 4879, 3.7, 0, 'This short desc mercury', 'This is full description mercury', 'en');
-INSERT INTO solar_system.planets (name, orbital_period, diameter, gravity, is_satellites, short_description, full_description, language_id) VALUES ('venus', 224.7, 12.104, 8.9, 0, 'This short desc venus', 'This is full description venus', 'en');
+INSERT INTO solar_system.planets (name, orbital_period, diameter, gravity, is_satellites,
+                                  short_description, full_description, language_id)
+SELECT * FROM (SELECT 'mercury', 88, 4879, 3.7, 0, 'This short desc mercury',
+                 'This is full description mercury', 'en') AS tmp
+                 WHERE NOT EXISTS (
+                     SELECT name FROM solar_system.planets WHERE
+                             name='mercury'
+                     ) LIMIT 1;
+
+INSERT INTO solar_system.planets (name, orbital_period, diameter, gravity, is_satellites,
+                                  short_description, full_description, language_id)
+SELECT * FROM (SELECT 'venus', 224.7, 12.104, 8.9, 0, 'This short desc venus',
+                      'This is full description venus', 'en') AS tmp
+WHERE NOT EXISTS (
+        SELECT name FROM solar_system.planets WHERE
+                name='venus'
+    ) LIMIT 1;
+
