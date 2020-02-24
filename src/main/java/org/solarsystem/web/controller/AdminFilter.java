f482a603a8e0991c5925b88fdb9f7a3220b26d37
VasylDmitrovich <Rapidvasyaalister@gmail.com>
2020-02-24 22:53:41 +0200

    Added filter to authorization page. If login and pass good welcome, otherwise bye bye

--- src/main/java/org/solarsystem/web/controller/AdminFilter.java
+++ src/main/java/org/solarsystem/web/controller/AdminFilter.java
@@ -9,39 +9,32 @@ import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebFilter(filterName = "AdminFilter", urlPatterns = {"/admin1"})
+@WebFilter(filterName = "AdminFilter", urlPatterns = {"/admin"})
 public class AdminFilter implements Filter {
     public void destroy() {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        /*chain.doFilter(req, resp);
+
         PrintWriter out = resp.getWriter();
+        resp.setContentType("text/html;charset=UTF-8");
+
         String userLogin = req.getParameter("loginEmail");
         String userPassword = req.getParameter("loginPassword");
 
         if (userLogin != null && userPassword != null) {
             UserRepository userRepository = new UserRepository();
             User user = userRepository.getUserByEmailByPassword(userLogin, userPassword);
-            System.out.println(user);
-            if (user != null) {
-                out.print("<h3>welcome ADMIN</h3>");
-//                chain.doFilter(req, resp);
-            } else {
-                out.println("<h4>Invalid Login</h4>");
-            }
-        }*/
 
-        /*if (userLogin.equals("admin@gmail.com") && userPassword.equals("admin")){
-            chain.doFilter(req,resp);
-        } else {
-            resp.setContentType("text/html;charset=UTF-8");
-            out.print("username or password error!");
-            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/html/index.html");
-            requestDispatcher.include(req,resp);
-            AuthorizationView authorizationView = new AuthorizationView();
-            out.println(authorizationView.getAuthorizationPage());
-        }*/
+            if (user != null) {
+                chain.doFilter(req, resp);
+            } else {
+                out.print("Username or password error!");
+                AuthorizationView authorizationView = new AuthorizationView();
+                out.println(authorizationView.getAuthorizationPage());
+            }
+        }
+
     }
 
     public void init(FilterConfig config) throws ServletException {
