9c7d36fe9b07825ef262d8b9adea9f664b8d2220
VasylDmitrovich <Rapidvasyaalister@gmail.com>
2020-02-25 13:10:51 +0200

    Add delete field to admin page

--- src/main/java/org/solarsystem/web/view/AdminView.java
+++ src/main/java/org/solarsystem/web/view/AdminView.java
@@ -5,35 +5,45 @@ import org.solarsystem.web.dao.entity.Planet;
 import java.util.List;
 
 public class AdminView {
-    public String getFullPage(List<Planet> planetNames){
+    public String getFullPage(List<Planet> planetNames, String loginEmail, String loginPassword){
         IndexSingleton indexSingleton = IndexSingleton.getInstance();
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("<h2 style=\"text-decoration-color: white\">We have planets:</h2>");
+        stringBuilder.append("<a href='/admin?loginEmail="+loginEmail+"&loginPassword="+loginPassword+
+                "'>Add New Employee</a>");
+        stringBuilder.append("<h1>Planets List</h1>");
+        stringBuilder.append("<table border='1' width='100%'");
+        stringBuilder.append("<tr>\n" +
+                "    <th>Id</th><th>Name</th><th>OrbitalPeriod</th><th>Diameter</th><th>Gravity</th>" +
+                "<th>IsSatellites</th><th>ShortDescription</th><th>FullDescription</th><th>LanguageId</th><th>Images</th>\n" +
+                "</tr>");
+
         for (Planet p : planetNames
              ) {
             StringBuilder stringBuilderImages = new StringBuilder();
-            List<String> list = p.getImage();
-            for (String s: list
+            List<String> listImages = p.getImage();
+            for (String s: listImages
                  ) {
                 stringBuilderImages.append(s+",");
             }
+            stringBuilder.append("<tr>" +
+                    "<td>"+p.getId()+"</td>"+
+                    "<td>"+p.getName()+"</td>"+
+                    "<td>"+p.getOrbitalPeriod()+"</td>"+
+                    "<td>"+p.getDiameter()+"</td>"+
+                    "<td>"+p.getGravity()+"</td>"+
+                    "<td>"+p.isSatellites()+"</td>"+
+                    "<td>"+p.getShortDescription()+"</td>"+
+                    "<td>"+p.getFullDescription()+"</td>"+
+                    "<td>"+p.getLanguageId()+"</td>"+
+                    "<td>"+stringBuilderImages+"</td>"+
+                    "<td><a href='EditServlet?id="+p.getId()+"'>edit</a></td>"+
+                    "<td><a href='/delete?id="+p.getId()+"&loginEmail="+loginEmail+
+                    "&loginPassword="+loginPassword+"'>delete</a></td>"+
+                    "</tr>");
 
-            stringBuilder.append("<div class=\"alert alert-primary\" role=\"alert\">\n" +
-                    "Planet{" +
-                            "id=" + p.getId() +"\n<br>"+
-                            ", name='" + p.getName() + '\'' +"\n<br>"+
-                            ", orbitalPeriod=" + p.getOrbitalPeriod() +"\n<br>"+
-                            ", diameter=" + p.getDiameter() +"\n<br>"+
-                            ", gravity=" + p.getGravity() +"\n<br>"+
-                            ", isSatellites=" + p.isSatellites() +"\n<br>"+
-                            ", shortDescription='" + p.getShortDescription() + '\'' +"\n<br>"+
-                            ", fullDescription='" + p.getFullDescription() + '\'' +"\n<br>"+
-                            ", languageId='" + p.getLanguageId() + '\'' +"\n<br>"+
-                            ", image=" + stringBuilderImages +"\n<br>"+
-                            '}'+
-                    "</div>");
         }
-
+        stringBuilder.append("</table>");
         String fullPage = stringBuilder.toString();
         return indexSingleton.getIndex().replace("<!--admin-->",fullPage);
     }
