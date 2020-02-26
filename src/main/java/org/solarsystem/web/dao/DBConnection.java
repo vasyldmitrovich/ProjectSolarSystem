25daa3a31e5e25214463fdf2011acdcf52e9918b
ivannaht <ivhontaryk@gmail.com>
2020-02-26 15:38:11 +0200

    change Style (addition information)

--- src/main/java/org/solarsystem/web/dao/DBConnection.java
+++ src/main/java/org/solarsystem/web/dao/DBConnection.java
@@ -1,30 +1,26 @@
 package org.solarsystem.web.dao;
 
-import org.apache.log4j.Logger;
-import org.solarsystem.web.controller.PlanetController;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection implements AutoCloseable {
-
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL="jdbc:mysql://localhost:3306/";
-    /*Change user and password for your DB.*/
+    static final String DB_URL="jdbc:mysql://localhost/";
+    /*Change
+    name data base,
+    user and
+    password for your DB.*/
     static final String nameDB = "solar_system";
     static final String user = "root";
-    static final String password = "rootStrong&serverTimezone=UTC";
+    static final String password = "6587";
     private Connection connection = null;
-    public static final Logger log = Logger.getLogger(DBConnection.class);
+
     public DBConnection(){
-        log.info("Connection is successfully");
         try {
             Class.forName(JDBC_DRIVER).newInstance();
         } catch (IllegalAccessException | InstantiationException | ClassNotFoundException e){
-            log.info("Connection do not successfully"+e);
-            //e.printStackTrace();
-
+            e.printStackTrace();
         }
     }
 
