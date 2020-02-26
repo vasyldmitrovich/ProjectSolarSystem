df44c526841f07b35589379c8590cee65b2ed2f2
VovaProtsiv <vovapro111@gmail.com>
2020-02-27 01:34:17 +0200

    change test(by Protsiv)

--- src/test/java/org/solarsystem/web/service/CalcDistanceByNasa.java
+++ src/test/java/org/solarsystem/web/service/CalcDistanceByNasa.java
@@ -6,15 +6,14 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class CalcDistanceByNasa {
+ class CalcDistanceByNasa {
 
-@Test
-    void testDistanceFromEarthToMars(){
-    DistanceCalculator distanceCalculator = new NasaJson();
+    @Test
+    void testDistanceFromEarthToMars() {
+        DistanceCalculator distanceCalculator = new NasaJson();
         LocalDate localDate = LocalDate.parse("2016-09-09", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         double distance = distanceCalculator.calculateDistance("mars", "earth", localDate);
-        double accuracy = 100 * Math.abs(distance - 1.4095169270123702E8) / 1.4095169270123702E8;
-        System.out.println(accuracy);
+        double accuracy = 100 * Math.abs(distance - 1.4095169270123702E8/149598000) / (1.4095169270123702E8/149598000);
         Assertions.assertTrue(accuracy <= 0.0001);
     }
 }
